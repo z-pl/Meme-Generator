f5b088b1e08c06b8847510c5b7685b0f8a588a66
ahad abdul <ahad.a2703@gmail.com>
2022-07-20 14:06:52 +1000

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-title">
+        <img src = "./images/Troll-Face.png"></img>
+        <h2 class = "header-text">Meme Generator</h2>
+      </div>
+      <div className="header-items">
+
+      </div>
+    </div>
+  )
+}
