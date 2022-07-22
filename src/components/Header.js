1fc1345d19bc6f196cd05946dfdf228405707917
ahad abdul <ahad.a2703@gmail.com>
2022-07-22 20:03:27 +1000

    added image randomizer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 
-import img from "../images/troll-face.png";
+import img from "../images/Troll-Face.png";
 import React from "react";
 
 export default function Header() {
   return (
       <div className="header-title">
-        <img className = "header-img" alt = "troll face" src = {img} />
+        <img className = "header-img" alt = "twhyroll face" src = {img} />
         <h2 className = "header-text">Meme Generator</h2>
       </div>
   )
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,42 @@
- import React from "react"
+import React from "react";
+import memesData from "../memesData";
+import _ from "lodash";
 
- export default function Meme() {
+export default function Meme() {
+
+  const {data:{memes}} = memesData;
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    memeUrl: "http://i.imgflip.com/1bij.jpg"
+  });
+
+
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    let randInt = _.random(0, memes.length - 1);
+
+    setMeme(prevMeme => {
+      return (
+        {
+          ...prevMeme,
+          memeUrl: memes[randInt].url
+        }
+      )
+    })
+
+  }
+
   return (
     <div className="meme-container">
       <form className="meme-form">
         <input id = "text-top" type = "text"></input>
         <input id = "text-bottom" type = "text"></input>
-        <button id = "meme-btn">Get a new meme image</button>
+        <button onClick = {handleClick} id = "meme-btn">Get a new meme image</button>
       </form>
+      <img className="meme-img" src = {meme.memeUrl}></img>
     </div>
   )
- }
+}
